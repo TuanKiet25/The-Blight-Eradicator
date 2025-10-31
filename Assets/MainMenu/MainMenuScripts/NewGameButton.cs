536f9f36149628c75f029b964804dfbddc645efe
tdat-le <tandatlxag1602@gmail.com>
2025-10-31 23:51:37 +0700

    main menu

---
+++ Assets/MainMenu/MainMenuScripts/NewGameButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewGameButton : MonoBehaviour
+{
+    private const string SaveKey = "LastSavedScene";
+    public void LoadSceneOnClick()
+    {
+        PlayerPrefs.SetString(SaveKey, "1.TheDecayingFringe");
+        PlayerPrefs.Save();
+
+        SceneManager.LoadScene("1.TheDecayingFringe");
+    }
+}
