06fdf4138cb6554d982d3cc8ef6ac0334dfeb15d
tdat-le <tandatlxag1602@gmail.com>
2025-11-03 00:21:24 +0700

    mainMenu

---
+++ Assets/MainMenu/MainMenuScripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private Button PauseButton;
+    [SerializeField] private Button ResumeButton;
+    [SerializeField] private Button SettingButton;
+    [SerializeField] private Button QuitButton;
+    [SerializeField] private GameObject PauseMenuPanel;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Awake()
+    {
+        PauseButton.onClick.AddListener(() =>
+        {
+            if (PauseMenuPanel != null)
+            {
+                Time.timeScale = 0f;
+                PauseMenuPanel.SetActive(true);
+            }
+        });
+        ResumeButton.onClick.AddListener(() =>
+        {
+            if (PauseMenuPanel != null)
+            {
+                Time.timeScale = 1f;
+                PauseMenuPanel.SetActive(false);
+            }
+        });
+        QuitButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("MainMenu");
+        });
+    }
+}
