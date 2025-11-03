81ac250b32dde717b122a92d9f4b0ed26a29d7b5
TuanKiet25 <kietntse183810@fpt.edu.vn>
2025-11-03 11:52:14 +0700

    IntroGame

---
+++ Assets/Scripts/DialogueLine.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+[System.Serializable]
+public struct DialogueLine
+{
+    public string speakerName; // Tên người nói (ví dụ: "Kael")
+
+    // Đánh dấu xem có phải là Người nói A không (người bên trái)
+    public bool isSpeakerA;
+
+    [TextArea(3, 10)] // Giúp ô gõ text trong Inspector to hơn
+    public string lineText; // Nội dung câu thoại  
+}
